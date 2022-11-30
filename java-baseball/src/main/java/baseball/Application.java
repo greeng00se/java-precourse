d7e05013a520ec48430f8dac6c9ad4d72ca5022e
greeng00se <thegreengoose7@gmail.com>
2022-11-30 16:35:22 +0900

    feat(BaseballGameController): 요청을 처리하는 클래스 추가

--- java-baseball/src/main/java/baseball/Application.java
+++ java-baseball/src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+import baseball.domain.BaseballGame;
+import baseball.domain.BaseballNumbersFactory;
+import baseball.service.BaseballGameService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGameController baseballGameController = new BaseballGameController(baseballGameService());
+        baseballGameController.run();
+    }
+
+    private static BaseballGameService baseballGameService() {
+        return new BaseballGameService(baseballGame(), baseballNumbersFactory());
+    }
+
+    private static BaseballGame baseballGame() {
+        return new BaseballGame();
+    }
+
+    private static BaseballNumbersFactory baseballNumbersFactory() {
+        return new BaseballNumbersFactory();
     }
 }
