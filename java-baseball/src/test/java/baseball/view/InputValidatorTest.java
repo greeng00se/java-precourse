2a6df9a00b5019a3b73f57f3d2f12daa2d557810
greeng00se <thegreengoose7@gmail.com>
2022-11-30 14:18:38 +0900

    fix(InputValidatorTest): import 형식 변경

--- java-baseball/src/test/java/baseball/view/InputValidatorTest.java
+++ java-baseball/src/test/java/baseball/view/InputValidatorTest.java
@@ -1,9 +1,8 @@
 package baseball.view;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
