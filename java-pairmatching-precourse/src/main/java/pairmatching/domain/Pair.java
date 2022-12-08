15f6752acb03ecf4f4d02368e76c12bd2e8555a0
greeng00se <thegreengoose7@gmail.com>
2022-12-08 22:42:10 +0900

    feat(Pair): 페어를 표현하는 클래스 추가

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Pair.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class Pair {
+
+    private final List<Crew> pairs;
+
+    public Pair(List<Crew> pairs) {
+        this.pairs = pairs;
+    }
+}
