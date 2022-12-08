dff481375ac9a5591de4fec44713218815600408
greeng00se <thegreengoose7@gmail.com>
2022-12-08 21:46:55 +0900

    feat(InputValidator): 정보를 검증하는 메서드 추가

--- java-pairmatching-precourse/src/main/java/pairmatching/view/InputValidator.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/view/InputValidator.java
@@ -1,15 +1,49 @@
 package pairmatching.view;
 
 import java.util.List;
+import java.util.regex.Pattern;
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
 
 public class InputValidator {
 
     private static final List<String> VALID_COMMAND = List.of("1", "2", "3", "Q");
     private static final String INVALID_COMMAND_MESSAGE = "올바르지 않은 커맨드입니다";
+    private static final Pattern VALID_INFORMATION_PATTERN = Pattern.compile("[가-힇]*, [가-힇0-9]*, [가-힇]*");
+    private static final String INVALID_INFORMATION_MESSAGE = "올바른 과정, 레벨, 미션 형식의 정보를 입력해주세요.";
+    private static final String INFORMATION_DELIMITER = ", ";
+    private static final int COURSE_INDEX = 0;
+    private static final int LEVEL_INDEX = 1;
+    private static final int MISSION_INDEX = 2;
 
     public void validateCommand(String command) {
         if (!VALID_COMMAND.contains(command)) {
             throw new IllegalArgumentException(INVALID_COMMAND_MESSAGE);
         }
+    }
+
+    public void validateInformation(String information) {
+        if (!VALID_INFORMATION_PATTERN.matcher(information).matches()) {
+            throw new IllegalArgumentException(INVALID_INFORMATION_MESSAGE);
+        }
+        String[] info = information.split(INFORMATION_DELIMITER);
+        validateCourse(info[COURSE_INDEX]);
+        validateLevel(info[LEVEL_INDEX]);
+        validateMission(info[MISSION_INDEX]);
+    }
+
+    private void validateCourse(String course) {
+        if (!Course.names().contains(course)) {
+            throw new IllegalArgumentException(INVALID_INFORMATION_MESSAGE);
+        }
+    }
+
+    private void validateLevel(String level) {
+        if (!Level.names().contains(level)) {
+            throw new IllegalArgumentException(INVALID_INFORMATION_MESSAGE);
+        }
+    }
+
+    private void validateMission(String mission) {
     }
 }
--- java-pairmatching-precourse/src/test/java/pairmatching/view/InputValidatorTest.java
+++ java-pairmatching-precourse/src/test/java/pairmatching/view/InputValidatorTest.java
@@ -3,6 +3,7 @@ package pairmatching.view;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -27,5 +28,26 @@ class InputValidatorTest {
     @ValueSource(strings = {"1", "2", "3", "Q"})
     void validateCommand_메서드는_올바르지_커맨드를_입력하면_예외를_던지지_않는다(String command) {
         assertThatNoException().isThrownBy(() -> sut.validateCommand(command));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "백엔드, 레벨1",
+            "프론트엔드, 레벨6, 오징어게임",
+            " , , "
+    })
+    void validateInformation_메서드는_올바른_입력양식이_아니라면_IllegalArguementExcpetion을_던진다(String information) {
+        Assertions.assertThatThrownBy(() -> sut.validateInformation(information))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 과정, 레벨, 미션 형식의 정보를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "백엔드, 레벨1, 오징어게임",
+            "백엔드, 레벨2, 자동차경주",
+    })
+    void validateInformation_메서드는_올바른_과정_레벨_미션_정보를_입력하면_예외를_던지지_않는다(String information) {
+        assertThatNoException().isThrownBy(() -> sut.validateInformation(information));
     }
 }
