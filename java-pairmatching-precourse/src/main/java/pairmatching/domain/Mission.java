6836052ef60778f7b1f76bc18588fda23621aa0c
greeng00se <thegreengoose7@gmail.com>
2022-12-08 22:41:33 +0900

    feat(Mission): 미션을 표현하는 클래스 추가

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+public class Mission {
+
+    private final Level level;
+    private final String name;
+
+    public Mission(Level level, String name) {
+        this.level = level;
+        this.name = name;
+    }
+}
