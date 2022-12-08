f7db3a85aa1af20a21c1f10244f30a857e8ef68d
greeng00se <thegreengoose7@gmail.com>
2022-12-08 14:23:53 +0900

    refactor(ChangeFactory): Change가 아닌 Map을 반환하도록 수정

--- java-vendingmachine-precourse/src/main/java/vendingmachine/domain/ChangeFactory.java
+++ java-vendingmachine-precourse/src/main/java/vendingmachine/domain/ChangeFactory.java
@@ -10,7 +10,7 @@ public class ChangeFactory {
     private static final int VALID_AMOUNT_REMAIN = 10;
     private static final String INVALID_AMOUNT_FORMAT_MESSAGE = "거스름돈은 10원 단위로 입력해야합니다.";
 
-    public Change generate(Integer amount) {
+    public Map<Coin, Integer> generate(Integer amount) {
         validate(amount);
         return generateChange(amount);
     }
@@ -21,13 +21,13 @@ public class ChangeFactory {
         }
     }
 
-    private Change generateChange(Integer amount) {
+    private Map<Coin, Integer> generateChange(Integer amount) {
         Map<Coin, Integer> change = new EnumMap<>(Coin.class);
         while (amount > ZERO) {
             int coin = Coin.pickRandomCoinLessThanOrEqualTo(amount);
             amount -= coin;
             change.merge(Coin.from(coin), PLUS_COUNT, Integer::sum);
         }
-        return new Change(change);
+        return change;
     }
 }
--- java-vendingmachine-precourse/src/test/java/vendingmachine/domain/ChangeFactoryTest.java
+++ java-vendingmachine-precourse/src/test/java/vendingmachine/domain/ChangeFactoryTest.java
@@ -1,8 +1,9 @@
 package vendingmachine.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -16,15 +17,19 @@ class ChangeFactoryTest {
 
     @Test
     void generate_메서드는_올바르지_않은_거스름돈_형식을_입력하면_IllegalArgumentException을_던진다() {
-        Assertions.assertThatThrownBy(() -> sut.generate(101))
+        assertThatThrownBy(() -> sut.generate(101))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("거스름돈은 10원 단위로 입력해야합니다.");
     }
 
     @Test
     void generate_메서드는_10원단위의_값을_입력하면_무작위로_거스름돈을_생성하여_반환한다() {
-        Change change = sut.generate(1000);
+        Map<Coin, Integer> change = sut.generate(1000);
 
-        assertThat(change.calculateSum()).isEqualTo(1000);
+        Integer sumValue = change.keySet().stream()
+                .map(coin -> coin.amountMultiplyBy(change.get(coin)))
+                .reduce(0, Integer::sum);
+
+        assertThat(sumValue).isEqualTo(1000);
     }
 }
