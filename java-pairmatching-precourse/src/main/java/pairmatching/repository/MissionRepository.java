40b3f9630cff15f61e32c03a728922905173f39f
greeng00se <thegreengoose7@gmail.com>
2022-12-10 21:50:17 +0900

    test(MissionRepository): 미션 레포지토리 테스트 추가

--- java-pairmatching-precourse/src/main/java/pairmatching/repository/MissionRepository.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/repository/MissionRepository.java
@@ -8,17 +8,24 @@ import pairmatching.domain.Mission;
 public class MissionRepository {
 
     private static final String INVALID_MISSION_MESSAGE = "미션이 존재하지 않습니다.";
+    private static final List<Mission> missions = new ArrayList<>();
 
-    private final List<Mission> missions = new ArrayList<>();
-
-    public void saveAll(List<Mission> missions) {
-        this.missions.addAll(missions);
+    public static void saveAll(List<Mission> mission) {
+        missions.addAll(mission);
     }
 
-    public Mission findByLevelAndName(Level level, String missionName) {
+    public static Mission findByLevelAndName(Level level, String name) {
         return missions.stream()
-                .filter(mission -> mission.equals(new Mission(level, missionName)))
+                .filter(mission -> mission.equals(new Mission(level, name)))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_MISSION_MESSAGE));
+    }
+
+    public static List<Mission> findAll() {
+        return new ArrayList<>(missions);
+    }
+
+    public static void clear() {
+        missions.clear();
     }
 }
---
+++ java-pairmatching-precourse/src/test/java/pairmatching/repository/MissionRepositoryTest.java
@@ -0,0 +1,60 @@
+package pairmatching.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static pairmatching.domain.Level.LEVEL1;
+import static pairmatching.domain.Level.LEVEL5;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import pairmatching.domain.Mission;
+
+@DisplayName("MissionRepository 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class MissionRepositoryTest {
+
+    @BeforeEach
+    void setup() {
+        MissionRepository.clear();
+    }
+
+    @Test
+    void saveAll_메서드는_입력받은_모든_미션을_저장한다() {
+        Mission mission1 = new Mission(LEVEL1, "야구게임");
+        Mission mission2 = new Mission(LEVEL5, "콜라 키우기");
+
+        MissionRepository.saveAll(List.of(mission1, mission2));
+
+        assertThat(MissionRepository.findAll()).hasSize(2);
+    }
+
+    @Test
+    void findByLevelAndName_메서드는_입력받은_레벨과_미션명에_해당되는_미션이_없다면_예외를_던진다() {
+        assertThatThrownBy(() -> MissionRepository.findByLevelAndName(LEVEL1, "거위"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("미션이 존재하지 않습니다.");
+    }
+
+    @Test
+    void findByLevelAndName_메서드는_입력받은_레벨과_미션명에_해당되는_미션을_반환한다() {
+        Mission mission = new Mission(LEVEL1, "야구게임");
+        MissionRepository.saveAll(List.of(mission));
+
+        Mission findMission = MissionRepository.findByLevelAndName(LEVEL1, "야구게임");
+
+        assertThat(findMission).isEqualTo(mission);
+    }
+
+    @Test
+    void findAll_메서드는_저장된_모든_미션을_반환한다() {
+        Mission mission1 = new Mission(LEVEL1, "야구게임");
+        Mission mission2 = new Mission(LEVEL5, "사이다 마시기");
+        MissionRepository.saveAll(List.of(mission1, mission2));
+
+        assertThat(MissionRepository.findAll()).containsExactly(mission1, mission2);
+    }
+}
