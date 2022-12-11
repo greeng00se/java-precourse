6b449033ba861251a4267e2a5d7446ce22ef348e
greeng00se <thegreengoose7@gmail.com>
2022-12-11 21:51:40 +0900

    feat(Line): 노선에 대한 클래스 추가

--- java-subway-path-precourse/src/main/java/subway/domain/LineRepository.java
+++ java-subway-path-precourse/src/main/java/subway/domain/LineRepository.java
@@ -1,5 +1,7 @@
 package subway.domain;
 
+import static java.text.MessageFormat.format;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,20 +9,28 @@ import java.util.Objects;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
+    private static final String INVALID_LINE_MESSAGE = "{0}호선은 존재하지 않습니다.";
 
-    public static List<Line> lines() {
+    public List<Line> lines() {
         return Collections.unmodifiableList(lines);
     }
 
-    public static void addLine(Line line) {
+    public void addLine(Line line) {
         lines.add(line);
     }
 
-    public static boolean deleteLineByName(String name) {
+    public boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
     }
 
-    public static void deleteAll() {
+    public void deleteAll() {
         lines.clear();
+    }
+
+    public Line findByName(String name) {
+        return lines().stream()
+                .filter(line -> line.equals(new Line(name)))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(format(INVALID_LINE_MESSAGE, name)));
     }
 }
