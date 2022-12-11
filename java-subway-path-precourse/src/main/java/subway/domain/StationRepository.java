7dfbcbe376d0c564786d26fd90e25ef7a5297cd8
greeng00se <thegreengoose7@gmail.com>
2022-12-11 21:51:47 +0900

    feat(Station): 역에 대한 클래스 추가

--- java-subway-path-precourse/src/main/java/subway/domain/StationRepository.java
+++ java-subway-path-precourse/src/main/java/subway/domain/StationRepository.java
@@ -1,26 +1,37 @@
 package subway.domain;
 
+import static java.text.MessageFormat.format;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
+
+    private static final String INVALID_STATION_MESSAGE = "{0}역은 존재하지 않습니다.";
     private static final List<Station> stations = new ArrayList<>();
 
-    public static List<Station> stations() {
+    public List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
 
-    public static void addStation(Station station) {
+    public void addStation(Station station) {
         stations.add(station);
     }
 
-    public static boolean deleteStation(String name) {
+    public boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
     }
 
-    public static void deleteAll() {
+    public void deleteAll() {
         stations.clear();
+    }
+
+    public Station findByName(String name) {
+        return stations().stream()
+                .filter(station -> station.equals(new Station(name)))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(format(INVALID_STATION_MESSAGE, name)));
     }
 }
