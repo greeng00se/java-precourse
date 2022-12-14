e363534721c1a3889a6bfa91fa6d72ff7dbe2a99
greeng00se <thegreengoose7@gmail.com>
2022-12-14 19:58:38 +0900

    feat(Course, Level): 정적 팩터리 메서드 추가

--- java-pairmatching/src/main/java/pairmatching/Application.java
+++ java-pairmatching/src/main/java/pairmatching/Application.java
@@ -1,7 +1,31 @@
 package pairmatching;
 
+import static pairmatching.controller.command.FrontControllerCommand.MATCHING;
+import static pairmatching.controller.command.FrontControllerCommand.QUIT;
+import static pairmatching.controller.command.FrontControllerCommand.RESET;
+import static pairmatching.controller.command.FrontControllerCommand.SEARCH;
+
+import pairmatching.controller.Controller;
+import pairmatching.controller.FrontController;
+import pairmatching.controller.MatchingController;
+import pairmatching.controller.ResetController;
+import pairmatching.controller.SearchController;
+import pairmatching.controller.command.ControllerMapper;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        Controller pairMatchingController = new FrontController(controllerMapper());
+        pairMatchingController.run();
+    }
+
+    private static ControllerMapper controllerMapper() {
+        ControllerMapper controllerMapper = new ControllerMapper();
+        controllerMapper.put(MATCHING, new MatchingController());
+        controllerMapper.put(SEARCH, new SearchController());
+        controllerMapper.put(RESET, new ResetController());
+        controllerMapper.put(QUIT, () -> {
+        });
+        return controllerMapper;
     }
 }
--- java-pairmatching/src/main/java/pairmatching/domain/Level.java
+++ java-pairmatching/src/main/java/pairmatching/domain/Level.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -7,11 +9,18 @@ public enum Level {
     LEVEL4("레벨4"),
     LEVEL5("레벨5");
 
+    private static final String INVALID_LEVEL_MESSAGE = "올바르지 않은 레벨명입니다.";
+
     private String name;
 
     Level(String name) {
         this.name = name;
     }
-
-    // 추가 기능 구현
+    
+    public static Level from(String name) {
+        return Arrays.stream(values())
+                .filter(level -> level.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_LEVEL_MESSAGE));
+    }
 }
